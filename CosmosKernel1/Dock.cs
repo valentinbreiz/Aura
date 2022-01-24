d55323edfb6039758ecf6718b766b6b0bcace2f6
valentinbreiz <valentinbreiz@gmail.com>
2022-01-24 00:49:30 +0000

    Port to .net5 + add fps

--- CosmosKernel1/Dock.cs
+++ CosmosKernel1/Dock.cs
@@ -1,4 +1,5 @@
 using Cosmos.Core;
+using Cosmos.HAL;
 using Cosmos.System;
 using nifanfa.CosmosDrawString;
 using System;
@@ -12,8 +13,21 @@ namespace CosmosKernel1
         uint Height = 30;
         uint Devide = 20;
 
+        int _frames = 0;
+        int _fps = 0;
+        int _deltaT = 0;
+
         public void Update()
         {
+            if (_deltaT != RTC.Second)
+            {
+                _fps = _frames;
+                _frames = 0;
+                _deltaT = RTC.Second;
+            }
+
+            _frames++;
+
             Width = (uint)(Kernel.apps.Count * Kernel.programlogo.Width + Kernel.apps.Count * Devide);
 
             Kernel.vMWareSVGAII.DoubleBuffer_DrawFillRectangle(0, 0, Kernel.screenWidth, 20, (uint)Kernel.avgCol.ToArgb());
@@ -21,6 +35,7 @@ namespace CosmosKernel1
             uint strX = 2;
             uint strY = (20 - 16) / 2;
             Kernel.vMWareSVGAII._DrawACSIIString("PowerOFF", (uint)Color.White.ToArgb(), strX, strY);
+            Kernel.vMWareSVGAII._DrawACSIIString("fps=" + _fps, (uint)Color.White.ToArgb(), (uint)((strX + 7) * ASC16.fontSize.Width), strY);
             string time = $"{DateTime.Now.Hour}:{DateTime.Now.Minute}";
             Kernel.vMWareSVGAII._DrawACSIIString(time, (uint)Color.White.ToArgb(), (uint)(Kernel.screenWidth - strX - time.Length * ASC16.fontSize.Width), strY);
             if (Kernel.Pressed)
